dfe8fd8297c9e79f49a5608d5828f355add40f10
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-02-02 20:56:31 -0300

    Atualização

--- PilotoApplication.java
+++ PilotoApplication.java
@@ -1,16 +1,5 @@
 package aplicativos;
 
-/*Adicione, ainda, as seguintes funcionalidades:
-- Cadastrar Piloto
-- Listar pilotos cadastrados
-- Localizar piloto pelo CPF.
-- Expandir o tamanho do vetor para que sua capacidade de
-armazenamento seja ampliada (não pode haver perda de informações).
-Para isso, solicite ao usuário o novo tamanho de armazenamento, crie um
-novo vetor desse tamanho, passe os itens de um vetor para o outro e
-realize todas as demais operações necessárias para concluir o processo e
-garantir que o programa continuará funcionando sem mais alterações.*/
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
